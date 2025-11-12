0a654db285ddda8fafde15f73a3e0aac443d8d36
Diego López Ariza <diegoawesome2017@outlook.com>
2025-11-12 11:42:59 -0600

    Servicio y modulo de prisma añadidos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config'
 import { OperationsModule } from './operations/operations.module';
+import { PrismaModule } from './prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), OperationsModule],
+  imports: [ConfigModule.forRoot(), OperationsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
