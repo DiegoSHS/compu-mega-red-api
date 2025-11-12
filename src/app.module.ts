f5c4c809ebf9f76f82a8649ac4faa3c6a5d526dd
Diego López Ariza <diegoawesome2017@outlook.com>
2025-11-12 11:37:30 -0600

    Modulo de operaciones creado

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config'
+import { OperationsModule } from './operations/operations.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), OperationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
