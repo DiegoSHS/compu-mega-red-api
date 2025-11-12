fbc3d3cbae1473022cbeeeef914f8d939679be4b
Diego López Ariza <diegoawesome2017@outlook.com>
2025-11-12 11:38:44 -0600

    Dto básico creado, aun sin validaciones

--- src/operations/dto/create-operation.dto.ts
+++ src/operations/dto/create-operation.dto.ts
@@ -1,1 +1,7 @@
-export class CreateOperationDto { }
+export class CreateOperationDto {
+    type: 'sale' | 'purchase';
+    amount: number;
+    date: Date;
+    user_id: string;
+    declaration_id?: string;
+}
